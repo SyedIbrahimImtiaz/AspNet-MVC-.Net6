90704bc5d0059c8ff8f94fc357afd01c9ddfe601
Ibrahim Imtiaz <Ibrahim Imtiaz@DESKTOP-P3E3GVO>
2022-01-25 16:29:28 +0500

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Core6.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
